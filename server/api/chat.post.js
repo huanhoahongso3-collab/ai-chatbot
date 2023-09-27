e8d00e9b7299aebdabad161a22fb1e0fba714be6
huanhoahongso3-collab <144003289+huanhoahongso3-collab@users.noreply.github.com>
2023-09-27 20:28:22 +0700

    Update chat.post.js

--- server/api/chat.post.js
+++ server/api/chat.post.js
@@ -5,14 +5,14 @@ export default defineEventHandler(async (event) => {
 	messages = messages.concat(previosMessages);
 	let prompt =
 		messages.map((message) => `${message.role}: ${message.message}`).join('\n') + `\nAI:`;
-	const req = await fetch('https://api.openai.com/v1/completions', {
+	const req = await fetch('https://api.nova-oss.com/v1', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
 			Authorization: `Bearer ${config.OPENAI_API_KEY}`
 		},
 		body: JSON.stringify({
-			model: 'text-davinci-003',
+			model: 'gpt-4',
 			prompt: prompt,
 			temperature: 0.9,
 			max_tokens: 512,
